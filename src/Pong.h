a0ecc62b8455acc36b33bb4f0026ae155e85c59c
Ritaban Chaudhuri <inrcr27@gmail.com>
2024-09-13 23:22:56 +0530

    Added Comments

--- src/Pong.h
+++ src/Pong.h
@@ -13,6 +13,7 @@
 class Pong
 {
 private:
+    //Vertex Array,Vertex Buffers, Index Buffers,Shaders,Renderer
     VertexArray m_VAO;
     VertexArray m_BallVAO;
     VertexBuffer* m_VBO;
@@ -22,10 +23,12 @@ private:
     Shader* m_Shader;
     Renderer* m_Renderer;
 
+    //Vec3 to store the posns
     glm::vec3 m_PaddleLeftPos;
     glm::vec3 m_PaddleRightPos;
     glm::vec3 m_BallPos;
 
+    //to store the velocity of the ball and the paddle
     glm::vec3 m_BallVelocity;
     float m_PaddleSpeed;
 
@@ -33,9 +36,11 @@ private:
     glm::mat4 m_Projection;
     glm::mat4 m_View;
 
-    // Helper functions
+    // Reset the Ball and Paddles
     void ResetBall();
     void ResetPaddles();
+
+    //Checking the Collisions
     void CheckCollisions();
 public:
 	Pong();
